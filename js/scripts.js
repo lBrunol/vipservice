ecd367872dd214149527e0c414fb3e1613a65dcd
Bruno Alves <bruno.ar.avl@gmail.com>
2018-10-23 22:25:45 -0300

    Classe modelo para posts e função para criar o objeto post. Tb alterei a permissão dos arquivos do tema.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,67 @@
-var t="0";
+class Post {
+    constructor(id, date, status, title, link, parent, order, price, hasChildren, imageUrl){
+        this.id = id;
+        this.date = date;
+        this.status = status;
+        this.title = title;
+        this.link = link;
+        this.parent = parent;
+        this.order = order;
+        this.price = price;
+        this.hasChildren = hasChildren;
+        this.imageUrl = imageUrl;
+    }
+} 
+let app = new Vue({
+    el: '.servicos-orcamento',
+    data: {
+        message : 'Olá',
+        loading: 'Carregando',
+        posts: []
+    },
+    created: function(){
+        this.debounceGetPosts = _.debounce(this.getPosts, 500);
+        this.debounceGetPosts();
+    },
+    methods: {
+        getPosts: function(){
+            let vm = this;
+            axios.get('/wp-json/wp/v2/servicos_orcamento?_embed')
+                .then(function(response){
+                    posts = vm.handlePosts(response.data);
+                    vm.posts = posts;
+                    vm.loading = 'Carregado';
+                })
+                .catch(function(error){
+                    console.log(error);
+                    vm.loading = 'Erro';
+                })
+        },
+        handlePosts: function(posts){
+            postsList = [];
+            if(Array.isArray(posts)){
+                posts.forEach(function(item){
+                    p = new Post();
+                    p.id = item.id;
+                    p.date = item.date;
+                    p.status = item.status;
+                    p.title = item.title.rendered;
+                    p.link = item.link;
+                    p.parent = item.parent;
+                    p.order = item.menu_order;
+                    p.price = item.servico_orcamento_preco;
+                    p.hasChildren = item.has_children;
+                    p.imageUrl = '';
+                    if(item._embedded !== undefined){
+                        if(item._embedded['wp:featuredmedia'] !== undefined){
+                            p.imageUrl = item._embedded['wp:featuredmedia'][0].source_url;
+                        }
+                    }
+                    postsList.push(p);
+                });
+                return postsList;
+            }
+            return posts;
+        }
+    }
+});
